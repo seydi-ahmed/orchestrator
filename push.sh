d39eb8977c84fa04050bcfff9ad8a50621287b45
mouhameddiouf <seydiahmedelcheikh@gmail.com>
2025-05-20 16:53:55 +0000

    .gitignore mis à jour

--- push.sh
+++ push.sh
@@ -2,5 +2,5 @@ git config --global user.email "seydiahmedelcheikh@gmail.com"
 git config --global user.name "mouhameddiouf"
 git config credential.helper store
 git add .
-git commit -m "troisiéme et derniére partie des questions réponses"
+git commit -m ".gitignore mis à jour"
 git push --force
